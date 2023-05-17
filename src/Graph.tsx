548a29e47718b8bb7ee8d9506f3739a0f4e870c7
D-R-Donovan <65715422+D-R-Donovan@users.noreply.github.com>
2023-05-17 16:27:37 +0100

    moved to prettier linting

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -1,7 +1,17 @@
 import React, { useRef, useEffect } from "react";
 
-type LinePlottingFunction = (ctx: CanvasRenderingContext2D, width: number, height: number, padding: number) => void;
-type AxisPlottingFunction = (ctx: CanvasRenderingContext2D, width: number, height: number, padding: number | undefined) => void;
+type LinePlottingFunction = (
+    ctx: CanvasRenderingContext2D,
+    width: number,
+    height: number,
+    padding: number
+) => void;
+type AxisPlottingFunction = (
+    ctx: CanvasRenderingContext2D,
+    width: number,
+    height: number,
+    padding: number | undefined
+) => void;
 
 interface graphProps {
     height: number;
@@ -28,26 +38,37 @@ function Graph(props: graphProps): JSX.Element {
         let padding = props.padding === undefined ? 20 : props.padding;
         let ctx: CanvasRenderingContext2D = ref.getContext("2d")!;
 
-        ctx.setTransform(1,0,0,1,0,0);
+        ctx.setTransform(1, 0, 0, 1, 0, 0);
         ctx.clearRect(0, -ref.height, ref.width, 2 * ref.height);
 
-        // find device pixel density and CSS pixel difference 
+        // find device pixel density and CSS pixel difference
         let dpr = window.devicePixelRatio || 1;
 
-        // change canvas origin to be in middle of canvas
-            if (!props.translate) {
-            } else if (Array.isArray(props.translate)) {
-                ctx.translate(props.translate[0], props.translate[1]);
-            } else if (props.translate === "center") {
-                ctx.translate(ref.width / 2, ref.height / 2)
-            } else if (props.translate === "midLeft") {
-                ctx.translate(0, ref.height / 2);
-            } else {
-                throw Error("translation argument is unrecognised")
-            }
+        // TODO: add an enum? or similar for supported translations
+        if (!props.translate) {
+        } else if (Array.isArray(props.translate)) {
+            ctx.translate(props.translate[0], props.translate[1]);
+        } else if (props.translate === "center") {
+            ctx.translate(ref.width / 2, ref.height / 2);
+        } else if (props.translate === "midLeft") {
+            ctx.translate(0, ref.height / 2);
+        } else {
+            throw Error("translation argument is unrecognised");
+        }
 
         if (!props.plotAxis) {
-            axisSetup(ctx, padding, ref.width, ref.height, props.xRange, props.yRange, props.intervalX, props.intervalY, props.unitsX, props.unitsY);
+            axisSetup(
+                ctx,
+                padding,
+                ref.width,
+                ref.height,
+                props.xRange,
+                props.yRange,
+                props.intervalX,
+                props.intervalY,
+                props.unitsX,
+                props.unitsY
+            );
         } else {
             props.plotAxis(ctx, ref.width, ref.height, padding);
         }
@@ -55,20 +76,36 @@ function Graph(props: graphProps): JSX.Element {
         props.plotLine(ctx, ref.width, ref.height, padding);
 
         // scale CSS pixel to device pixel
-        ctx.scale(dpr, dpr);;
-    })
+        ctx.scale(dpr, dpr);
+    });
 
-    return <canvas ref={localref} className={props.className} height={props.height} width={props.width} />
+    return (
+        <canvas
+            ref={localref}
+            className={props.className}
+            height={props.height}
+            width={props.width}
+        />
+    );
 }
 
-function axisSetup(ctx: CanvasRenderingContext2D, padding: number, width: number, height: number,
-    xRange: [number, number] | undefined, yRange: [number, number] | undefined, intervalX: number = 1, intervalY: number = 1, unitsX: string = "", unitsY: string = "") {
-
-    ctx.beginPath()
-    ctx.lineWidth = 1
+function axisSetup(
+    ctx: CanvasRenderingContext2D,
+    padding: number,
+    width: number,
+    height: number,
+    xRange: [number, number] | undefined,
+    yRange: [number, number] | undefined,
+    intervalX: number = 1,
+    intervalY: number = 1,
+    unitsX: string = "",
+    unitsY: string = ""
+) {
+    ctx.beginPath();
+    ctx.lineWidth = 1;
     ctx.strokeStyle = "#b281ff";
-    ctx.textAlign = "center"
-    ctx.fillStyle = "white"
+    ctx.textAlign = "center";
+    ctx.fillStyle = "white";
 
     let minGraphX = padding;
     let maxGraphX = width - padding;
@@ -85,24 +122,23 @@ function axisSetup(ctx: CanvasRenderingContext2D, padding: number, width: number
     ctx.moveTo(minGraphX, 0);
     ctx.lineTo(maxGraphX, 0);
 
-    ctx.font = "13px Arial"
+    ctx.font = "13px Arial";
     for (let x = 0; x <= 8; x++) {
-        const xPos = (x * ((maxGraphX - minGraphX) / 8)) + minGraphX;
-        ctx.moveTo(xPos, + 5)
-        ctx.lineTo(xPos, - 5)
+        const xPos = x * ((maxGraphX - minGraphX) / 8) + minGraphX;
+        ctx.moveTo(xPos, +5);
+        ctx.lineTo(xPos, -5);
 
-        ctx.fillText(String(x / 4) + unitsX, xPos, 20)
+        ctx.fillText(String(x / 4) + unitsX, xPos, 20);
     }
 
-
-    ctx.font = "10px Arial"
+    ctx.font = "10px Arial";
     for (let y = 0; y <= 4; y++) {
         const yPos = (-1 + 0.5 * y) * maxGraphY;
 
-        ctx.moveTo(padding - 5, yPos)
-        ctx.lineTo(padding, yPos)
+        ctx.moveTo(padding - 5, yPos);
+        ctx.lineTo(padding, yPos);
 
-        ctx.fillText(String(1 - 0.5 * y) + unitsY, 10, yPos + 13)
+        ctx.fillText(String(1 - 0.5 * y) + unitsY, 10, yPos + 13);
     }
     ctx.stroke();
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { Header } from './Header';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { Header } from "./Header";
+import Unitcircle from "./Unitcircle";
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <div className='top-grid'>
-      <Header />
-      <App />
-    </div>
-  </React.StrictMode>
+    <React.StrictMode>
+        <div className="top-grid">
+            <Header />
+            <Unitcircle />
+            <App />
+        </div>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
